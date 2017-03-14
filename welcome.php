d522c8fbfa802817522f28081f85ebd16f11e12c
nibrocd <nibrocd@gmail.com>
2017-03-14 10:46:42 -0500

    remove dead chat

--- welcome.php
+++ welcome.php
@@ -40,69 +40,6 @@
     
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script type="text/javascript" src="chat.js"></script>
-    <script type="text/javascript">
-    
-        // ask user for name with popup prompt    
-        //var name = prompt("Enter your chat name:", "Guest");
-		var name = "<?php echo $login_session ?>";
-        
-    	
-    	// strip tags
-    	name = name.replace(/(<([^>]+)>)/ig,"");
-    	
-    	// display name on page
-    	$("#name-area").html("You are: <span>" + name + "</span>");
-    	
-    	// kick off chat
-        var chat =  new Chat();
-    	$(function() {
-    	
-    		 chat.getState(); 
-    		 
-    		 // watch textarea for key presses
-             $("#sendie").keydown(function(event) {  
-             
-                 var key = event.which;  
-           
-                 //all keys including return.  
-                 if (key >= 33) {
-                   
-                     var maxLength = $(this).attr("maxlength");  
-                     var length = this.value.length;  
-                     
-                     // don't allow new content if length is maxed out
-                     if (length >= maxLength) {  
-                         event.preventDefault();  
-                     }  
-                  }  
-    		 																																																});
-    		 // watch textarea for release of key press
-    		 $('#sendie').keyup(function(e) {	
-    		 					 
-    			  if (e.keyCode == 13) { 
-    			  
-                    var text = $(this).val();
-    				var maxLength = $(this).attr("maxlength");  
-                    var length = text.length; 
-                     
-                    // send 
-                    if (length <= maxLength + 1) { 
-                     
-    			        chat.send(text, name);	
-    			        $(this).val("");
-    			        
-                    } else {
-                    
-    					$(this).val(text.substring(0, maxLength));
-    					
-    				}	
-    				
-    				
-    			  }
-             });
-            
-    	});
-    </script>
 </head>
 
 <body onload="setInterval('chat.update()', 1000)">
