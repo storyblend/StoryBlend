6af9272a574fe6f118d779a71c21419297f6b526
Collin <muhls@highlandcusd5.org>
2017-03-23 11:01:40 -0500

    new sql tables

--- SQL TABLES/id695241_storyblenddb.sql
+++ SQL TABLES/id695241_storyblenddb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2017 at 04:48 PM
+-- Generation Time: Mar 23, 2017 at 05:01 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.5.38
 
@@ -31,23 +31,8 @@ CREATE TABLE `posts` (
   `story_list_id` int(11) NOT NULL,
   `owned_by_id` int(50) NOT NULL,
   `post` varchar(5000) NOT NULL,
-  `timestamp` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `author` varchar(100) NOT NULL
+  `timestamp` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ;
-
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id_post`, `story_list_id`, `owned_by_id`, `post`, `timestamp`, `author`) VALUES
-(12, 9, 1, 'refresh test', '2017-02-21 16:29:22.633687', ''),
-(17, 15, 1, 'A woman was driving back to her home in California from Washington to California. It was late evening and snowhad begun to fall before she finally reached the little Oregon town where she planed tospend the night. Tired and ready for a hot meal and a goodnightâ€™s sleep, she stopped at the first pla', '2017-02-21 16:53:25.146153', ''),
-(28, 14, 1, 'THe man on the mooon', '2017-03-07 17:02:34.104169', ''),
-(29, 14, 1, 'the cow jumped over the moon', '2017-03-07 17:02:41.742723', ''),
-(30, 15, 1, 'A spooky hotel manager came out of the door and said "HARRO MEESTER DINBEY"', '2017-03-07 17:10:33.193570', ''),
-(36, 18, 5, 'hi there, my name jeff', '2017-03-14 15:55:09.599858', ''),
-(37, 19, 5, 'epic win\r\n', '2017-03-14 16:03:10.417300', ''),
-(38, 19, 5, 'test', '2017-03-14 16:10:30.004128', 'testaccount');
 
 -- --------------------------------------------------------
 
@@ -57,8 +42,9 @@ INSERT INTO `posts` (`id_post`, `story_list_id`, `owned_by_id`, `post`, `timesta
 
 CREATE TABLE `story_list` (
   `id` int(50) NOT NULL,
-  `created_by_id` int(50) NOT NULL,
+  `created_by_id` varchar(50) NOT NULL,
   `shared_with` varchar(50) NOT NULL,
+  `turn` varchar(20) NOT NULL,
   `story_name` varchar(100) NOT NULL,
   `story_description` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -67,15 +53,8 @@ CREATE TABLE `story_list` (
 -- Dumping data for table `story_list`
 --
 
-INSERT INTO `story_list` (`id`, `created_by_id`, `shared_with`, `story_name`, `story_description`) VALUES
-(14, 1, 'name', 'The man on the moon', 'A story about a man who is stuck on the moon.'),
-(15, 1, 'name', 'Welcome to the hotel of deathhhh', 'a spooky hotel is the spookiest spook ever'),
-(16, 1, 'collinbonker', 'sharing this', 'testing'),
-(17, 1, 'test', 'test sharing', 'yup'),
-(18, 5, 'asdf', 'asdf', 'asdf'),
-(19, 5, 'testaccount', 'Hi', 'My name is jeff'),
-(20, 1, 'test', 'test', 'test'),
-(21, 1, 'sql inj', 'sql inj', 'sql inj');
+INSERT INTO `story_list` (`id`, `created_by_id`, `shared_with`, `turn`, `story_name`, `story_description`) VALUES
+(2, 'test', 'memeboy', 'test', 'turns lets go', 'lets do it');
 
 -- --------------------------------------------------------
 
@@ -125,7 +104,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `story_list`
 --
 ALTER TABLE `story_list`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user_info`
 --
